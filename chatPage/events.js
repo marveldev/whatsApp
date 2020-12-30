68bbfc4efb72fff4378a7b71bfacbbe5b0dc4716
Marvel Dev <marvellousalika48@gmail.com>
2020-12-30 14:51:13 +0100

    initial commit

---
+++ chatPage/events.js
@@ -0,0 +1,10 @@
+import chatPage from "./chatPage.js"
+
+const chatPageEventListeners = () => {
+  const chatButton = document.querySelector('#chatButton')
+  chatButton.addEventListener('click', () => {
+    document.querySelector('.current-page').innerHTML = chatPage
+  })
+}
+
+export default chatPageEventListeners
