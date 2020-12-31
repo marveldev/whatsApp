6051c86d11b0e4512e18faca442c5860724c3cac
Marvel Dev <marvellousalika48@gmail.com>
2020-12-31 06:51:18 +0100

    add edit icon and camera icon button container

--- statusPage/statusPage.js
+++ statusPage/statusPage.js
@@ -2,6 +2,10 @@ const statusPage = () => {
   return `
     <div class="status-page">
       <div>
+        <button class="edit-icon"><i class="material-icons">&#xe3c9;</i></button>
+        <button class="camera icon"><i class="fa fa-camera"></i></button>
+      </div>
+      <div>
         <button class="chat-profile">
           <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
             class="image" alt="photo">
