51b42e465940acf8acbb56c9ca57e138ba489b1e
Marvel Dev <marvellousalika48@gmail.com>
2020-12-31 06:50:03 +0100

    add call icon button container

--- callsPage/callsPage.js
+++ callsPage/callsPage.js
@@ -2,7 +2,12 @@ const callsPage = () => {
   return `
     <div class="calls-page">
       <div>
-        <p>To start calling contacts who have WhatsApp, tap at the bottom of your screeen</p>
+        <button class="call icon"><i class="fa fa-phone"></i></button>
+      </div>
+      <div>
+        <p>To start calling contacts who have WhatsApp, tap
+          <i class="fa fa-phone"></i> at the bottom of your screeen
+        </p>
       </div>
     </div>
   `
