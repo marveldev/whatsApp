a792d5efe41b83952a6683f8c90c283b44c8109c
Marvel Dev <marvellousalika48@gmail.com>
2020-12-31 05:47:03 +0100

    add parenthesis to chat page

--- chatPage/events.js
+++ chatPage/events.js
@@ -3,7 +3,7 @@ import chatPage from "./chatPage.js"
 const chatPageEventListeners = () => {
   const chatButton = document.querySelector('#chatButton')
   chatButton.addEventListener('click', () => {
-    document.querySelector('.current-page').innerHTML = chatPage
+    document.querySelector('.current-page').innerHTML = chatPage()
   })
 }
 
