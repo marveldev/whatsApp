000583c8080b2cd8d9e9e67651ffb52ab1b308ff
Marvel Dev <marvellousalika48@gmail.com>
2020-12-31 05:48:41 +0100

    add chat profile button container

--- statusPage/statusPage.js
+++ statusPage/statusPage.js
@@ -1,7 +1,16 @@
 const statusPage = () => {
   return `
     <div class="status-page">
-      <h1>hello</h1>
+      <div>
+        <button class="chat-profile">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="chats">
+            <strong>My status</strong>
+            <p>Tap to add status update</p>
+          </div>
+        </button>
+      </div>
     </div>
   `
 }
