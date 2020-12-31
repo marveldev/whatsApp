dc48c46ce2590c5a7997826be45e23bbf2d0277d
Marvel Dev <marvellousalika48@gmail.com>
2020-12-31 05:47:27 +0100

    add parenthesis to statusPage

--- statusPage/events.js
+++ statusPage/events.js
@@ -3,7 +3,7 @@ import statusPage from "./statusPage.js"
 const statusPageEventListeners = () => {
   const statusButton = document.querySelector('#statusButton')
   statusButton.addEventListener('click', () => {
-    document.querySelector('.current-page').innerHTML = statusPage
+    document.querySelector('.current-page').innerHTML = statusPage()
   })
 }
 
