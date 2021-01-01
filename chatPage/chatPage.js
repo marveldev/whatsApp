345333fc23e53cd3215a3ad265a121ed3e588258
Marvel Dev <marvellousalika48@gmail.com>
2021-01-01 14:31:41 +0100

    add user button containers

--- chatPage/chatPage.js
+++ chatPage/chatPage.js
@@ -74,6 +74,46 @@ const chatPage = () => {
           </div>
         </div>
       </button>
+      <button class="chat-profile">
+        <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+          class="image" alt="photo">
+        <div class="chats">
+          <strong>Jack Williams</strong>
+          <div>
+            <p>Hello</p>
+          </div>
+        </div>
+      </button>
+      <button class="chat-profile">
+        <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+          class="image" alt="photo">
+        <div class="chats">
+          <strong>Jack Williams</strong>
+          <div>
+            <p>Hello</p>
+          </div>
+        </div>
+      </button>
+      <button class="chat-profile">
+        <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+          class="image" alt="photo">
+        <div class="chats">
+          <strong>Jack Williams</strong>
+          <div>
+            <p>Hello</p>
+          </div>
+        </div>
+      </button>
+      <button class="chat-profile">
+        <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+          class="image" alt="photo">
+        <div class="chats">
+          <strong>Jack Williams</strong>
+          <div>
+            <p>Hello</p>
+          </div>
+        </div>
+      </button>
     </div>
   `
 }
