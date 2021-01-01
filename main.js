f7d2cea8434bfeb166ca64614cb271c6cd31a3ed
Marvel Dev <marvellousalika48@gmail.com>
2021-01-01 14:58:02 +0100

    rename chatPage to default page
    - add defaultpage event listeners

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 import topNav from "./topNav/topNav.js"
-import chatPage from "./chatPage/chatPage.js"
+import defaultPage from "./defaultPage/defaultPage.js"
 import pageEventListeners from "./helper.js"
+import defaultPageEventListeners from "./defaultPage/events.js"
 
 const main = () => {
   return `
     ${topNav()}
     <div class="current-page">
-      ${chatPage()}
+      ${defaultPage()}
     </div>
   `
 }
 
 document.querySelector('.main').innerHTML = main()
+defaultPageEventListeners()
 pageEventListeners()
